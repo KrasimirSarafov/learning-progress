4c8e8582c92784b91af9406081d28e7e4e64f7ff
KrasimirSarafov <krasimirsarafov04@abv.bg>
2026-07-28 01:46:51 +0300

    Update comment to reflect correct username

---
+++ If Statement.py
@@ -0,0 +1,9 @@
+# Dave tries to fool me with fake username
+user_name = "angela_catlady_87"
+
+if user_name == "Dave":
+  print("Get off my computer Dave!")
+
+  if user_name == "angela_catlady_87":
+    print("I know it is you, Dave! Go away!")
+
