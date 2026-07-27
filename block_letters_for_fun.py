e0e9a4c8cf0980210cc055681b23823836053a61
KrasimirSarafov <krasimirsarafov04@abv.bg>
2026-07-28 01:42:36 +0300

    Create block_letters_for_fun.py for name display
    Added a script to print block letters for the name 'Krasimir Krasimirov Sarafov'.

---
+++ block_letters_for_fun.py
@@ -0,0 +1,28 @@
+#My full name is Krasimir Krasimirov Sarafov
+#Block letters initial
+
+print("k   k")
+print("k  K")
+print("k k")
+print("kk")
+print("k k")
+print("k  k")
+print("k   K")
+
+
+print("k   k")
+print("k  K")
+print("k k")
+print("kk")
+print("k k")
+print("k  k")
+print("k   K")
+
+
+print(" sss ")
+print("s   s")
+print("s    ")
+print(" sss ")
+print("    s")
+print("s   s")
+print(" sss ")
