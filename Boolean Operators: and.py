757e66f39213e5b36b2f101aa054ac5e161104e1
KrasimirSarafov <krasimirsarafov04@abv.bg>
2026-07-29 23:11:15 +0300

    Add Boolean operators exercises in and.py

---
+++ Boolean Operators: and.py
@@ -0,0 +1,15 @@
+#"And" exercises:
+
+#1. exercises
+statement_one = (2 + 2 + 2 >= 6) and (-1 * -1 < 0)
+
+statement_two = (4 * 2 <= 8) and (7 - 1 == 6)
+
+#2. exercises
+credits = 120
+gpa = 3.4
+
+if credits >= 120 and gpa >= 2.0:
+  print("You meet the requirements to graduate!")
+
+  
